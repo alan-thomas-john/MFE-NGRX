578ced5116c95ba12001f5839f41b17be3203380
Gopika O S (intern) <gopika.os@bytestrone.com>
2024-06-19 11:18:08 +0530

    new login commit

--- OneByte-WorkSpace/projects/one-byte/src/app/app-routing.module.ts
+++ OneByte-WorkSpace/projects/one-byte/src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'home',component:HomeComponent},
 ];
+
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
