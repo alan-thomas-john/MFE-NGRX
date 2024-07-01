74473a4377dfcb4af33a49089e50e57afc04d181
Gopika O S (intern) <gopika.os@bytestrone.com>
2024-07-01 15:26:40 +0530

    css

--- node/mfeProject/config/db.js
+++ node/mfeProject/config/db.js
@@ -1,29 +1,7 @@
 
-
-// const { Pool } = require('pg');
-
-// // Create a new pool instance with your database configuration
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'oneByte',
-//   password: 'fahim',
-//   port: 5432,
-// });
-
-// // Function to query the database
-// const query = (text, params) => {
-//   return pool.query(text, params);
-// };
-
-// // Export the query function and the pool instance if needed
-// module.exports = {
-//   query,
-//   pool
-// };
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('oneByte', 'postgres', 'fahim', {
+const sequelize = new Sequelize('OneByte', 'postgres', 'Manju@12345', {
   host: 'localhost',
   dialect: 'postgres',
 });
