034b34d17126dada0445626c9da5a5fa7de75bf9
Gopika O S (intern) <gopika.os@bytestrone.com>
2024-07-08 18:22:04 +0530

    tailwindcss configuration done completely

--- OneByte-WorkSpace/projects/employee-management/src/employee-list/employee-list.component.ts
+++ OneByte-WorkSpace/projects/employee-management/src/employee-list/employee-list.component.ts
@@ -4,17 +4,9 @@ import { Employee } from '../app/state/employee.model';
 import { Store, select } from '@ngrx/store';
 import { EmployeeState } from '../app/state/employee.reducer';
 import { selectAllEmployees } from '../app/state/employee.selectors';
-import {
-  deleteEmployee,
-  deleteEmployeeSuccess,
-  setEmployees,
-} from '../app/state/employee.actions';
+import { deleteEmployee, setEmployees } from '../app/state/employee.actions';
 import { EmployeeService } from '../app/state/employee.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { AfterViewInit, ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-employee-list',
@@ -25,6 +17,7 @@ export class EmployeeListComponent implements OnInit {
   employees$: Observable<Employee[]> | undefined;
   employees: any;
   isEmpty: boolean = true;
+  employee: any;
 
   constructor(
     private store: Store<{ employees: EmployeeState }>,
@@ -57,7 +50,7 @@ export class EmployeeListComponent implements OnInit {
     });
   }
 
-  deleteEmployees(id:number) {
-    this.store.dispatch(deleteEmployee({id}))
+  deleteEmployees(id: number) {
+    this.store.dispatch(deleteEmployee({ id }));
   }
 }
